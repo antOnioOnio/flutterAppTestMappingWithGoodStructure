4e44ee6fda46f00e2839f4d35b1cd8f6c03631a3
antOnioOnio <anthoniusstanfield@gmail.com>
2020-10-06 11:47:59 +0200

    Changes dependencies to moor

--- lib/database/converters/BartenderPositionsConverter.dart
+++ lib/database/converters/BartenderPositionsConverter.dart
@@ -1,7 +1,14 @@
 import 'package:test_mapping/database/models/ModelBartenderPosition.dart';
-
 /*
+
 class BartenderPositionConverter extends TypeConverter<DateTime, int> {
+  @override
+  DateTime decode(int databaseValue) {
+    return DateTime.fromMillisecondsSinceEpoch(databaseValue);
+  }
 
-
+  @override
+  int encode(DateTime value) {
+    return value.millisecondsSinceEpoch;
+  }
 }*/
